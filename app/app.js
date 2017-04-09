0cfa69a7f23a826208c98ff3bc9dbbe63ee2c399
backtodeath <lirolaluis@gmail.com>
2017-04-10 01:11:23 +0300

    Clean up

--- app/app.js
+++ app/app.js
@@ -17,47 +17,72 @@ angular.module('myApp', [
                 controller: 'HomeCtrl'
             }).when('/bible', {
             template: '<div></div>',
-            controller: ['$window', function ($window) {
+            controller: ['$window', 'Analytics', function ($window, Analytics) {
                 angular.element(document).ready(function () {
                     $window.open(
                         'tizenstore://ProductDetail/000000086472', '_blank'
                     );
+                    Analytics.send({
+                        hitType: 'event',
+                        eventCategory: 'bible',
+                        eventAction: 'Redirect'
+                    });
                 });
             }]
         }).when('/captain', {
             template: '<div></div>',
-            controller: ['$window', function ($window) {
+            controller: ['$window', 'Analytics', function ($window, Analytics) {
                 angular.element(document).ready(function () {
                     $window.open(
                         'tizenstore://ProductDetail/000000089387', '_blank'
                     );
+                    Analytics.send({
+                        hitType: 'event',
+                        eventCategory: 'captain',
+                        eventAction: 'Redirect'
+                    });
                 });
             }]
         }).when('/football', {
             template: '<div></div>',
-            controller: ['$window', function ($window) {
+            controller: ['$window', 'Analytics', function ($window, Analytics) {
                 angular.element(document).ready(function () {
                     $window.open(
                         'tizenstore://ProductDetail/000000086304', '_blank'
                     );
+                    Analytics.send({
+                        hitType: 'event',
+                        eventCategory: 'football',
+                        eventAction: 'Redirect'
+                    });
                 });
             }]
         }).when('/calc', {
             template: '<div></div>',
-            controller: ['$window', function ($window) {
+            controller: ['$window', 'Analytics', function ($window, Analytics) {
                 angular.element(document).ready(function () {
                     $window.open(
                         'tizenstore://ProductDetail/000000089386', '_blank'
                     );
+                    Analytics.send({
+                        hitType: 'event',
+                        eventCategory: 'calc',
+                        eventAction: 'Redirect'
+                    });
                 });
             }]
         }).when('/lazors', {
             template: '<div></div>',
-            controller: ['$window', function ($window) {
+            controller: ['$window', 'Analytics', function ($window, Analytics) {
                 angular.element(document).ready(function () {
                     $window.open(
                         'tizenstore://ProductDetail/000000090674', '_blank'
                     );
+                });
+                Analytics.send({
+                    hitType: 'event',
+                    eventCategory: 'lazors',
+                    eventAction: 'Redirect'
                 });
             }]
         }).otherwise({
--- app/views/home.controller.js
+++ app/views/home.controller.js
@@ -2,25 +2,50 @@
 
 angular.module('myApp.home', [])
 
-.controller('HomeCtrl', ['$window', '$scope','$rootScope', function ($window, $scope, $rootScope) {
+.controller('HomeCtrl', ['$window', '$scope', 'Analytics', function ($window, $scope, Analytics) {
     $scope.bibleOpen = function () {
+        Analytics.send({
+            hitType: 'event',
+            eventCategory: 'bible',
+            eventAction: 'Click'
+        });
         openLink('000000086472');
     };
 
     $scope.lazorsOpen = function () {
+        Analytics.send({
+            hitType: 'event',
+            eventCategory: 'lazors',
+            eventAction: 'Click'
+        });
         openLink('000000090674');
     };
 
     $scope.footballOpen = function () {
+        Analytics.send({
+            hitType: 'event',
+            eventCategory: 'football',
+            eventAction: 'Click'
+        });
         openLink('000000086304');
     };
 
     $scope.captainOpen = function () {
+        Analytics.send({
+            hitType: 'event',
+            eventCategory: 'captain',
+            eventAction: 'Click'
+        });
         openLink('000000089387');
     };
 
 
     $scope.calcOpen = function () {
+        Analytics.send({
+            hitType: 'event',
+            eventCategory: 'calc',
+            eventAction: 'Click'
+        });
         openLink('000000089386');
     };
 
