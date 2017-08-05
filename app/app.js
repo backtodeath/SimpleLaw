c02e618ffdc8f6590f77951b0b4c5f56c6afef31
backtodeath <lirolaluis@gmail.com>
2017-08-06 00:12:26 +0300

    Update project

--- app/app.js
+++ app/app.js
@@ -3,6 +3,8 @@
 
 // Declare app level module which depends on views, and components
     angular.module('simpleLawApp', [
+        'mobile-angular-ui',
+        'mobile-angular-ui.gestures',
         'ngRoute',
         'angular-google-analytics'
     ]).config(['AnalyticsProvider', function (AnalyticsProvider) {
